68b734e1ce7ea1a7e62d1e32ea1264310b06dcd9
rohtas <rohtasprajapati@gmail.com>
2026-09-04 17:17:34 +0530

    monika

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 3 deployed automatically via GitHub Actions"
+    return "Monika I Love you"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
